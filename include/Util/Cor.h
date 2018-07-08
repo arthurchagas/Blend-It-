875eedb8c7bbafe2c707311be1647eed8c5abb9f
Arthur Chagas <744344@comp.ufscar.br>
2018-07-08 02:55:18 -0300

    Add comments to color util

--- include/Util/Cor.h
+++ include/Util/Cor.h
@@ -5,14 +5,15 @@
 
 #include <SFML/Graphics/Color.hpp>
 
+/* Geradores de cor aleatória */
 sf::Color cor_aleatoria_rgb(std::default_random_engine &gerador);
 sf::Color cor_aleatoria_rg(std::default_random_engine &gerador, unsigned b);
 sf::Color cor_aleatoria_rb(std::default_random_engine &gerador, unsigned g);
 sf::Color cor_aleatoria_gb(std::default_random_engine &gerador, unsigned r);
 
+/* Manipuladores de cor */
 sf::Color misturar(sf::Color a, sf::Color b);
 sf::Color inverter(sf::Color a);
-
 double distancia(sf::Color a, sf::Color b);
 
 #endif //UTIL_COR_H
--- src/Util/Cor.cpp
+++ src/Util/Cor.cpp
@@ -1,7 +1,9 @@
 #include "Util/Cor.h"
 
+// Distribuição uniforme, de 0 a 255, utilizada pela classe
 std::uniform_int_distribution<int> rgb(0, 255);
 
+// Gera uma cor cujas componentes são todas aleatórias
 sf::Color cor_aleatoria_rgb(std::default_random_engine &gerador)
 {
     sf::Color aux;
@@ -13,6 +15,8 @@ sf::Color cor_aleatoria_rgb(std::default_random_engine &gerador)
     return aux;
 }
 
+// Gera uma cor cujas componentes r (red) e g (green) são aleatórias
+// A componente b (blue) é constante
 sf::Color cor_aleatoria_rg(std::default_random_engine &gerador, unsigned b)
 {
     sf::Color aux;
@@ -24,6 +28,8 @@ sf::Color cor_aleatoria_rg(std::default_random_engine &gerador, unsigned b)
     return aux;
 }
 
+// Gera uma cor cujas componentes r (red) e b (blue) são aleatórias
+// A componente g (green) é constante
 sf::Color cor_aleatoria_rb(std::default_random_engine &gerador, unsigned g)
 {
     sf::Color aux;
@@ -35,6 +41,8 @@ sf::Color cor_aleatoria_rb(std::default_random_engine &gerador, unsigned g)
     return aux;
 }
 
+// Gera uma cor cujas componentes g (green) e b (blue) são aleatórias
+// A componente r (red) é constante
 sf::Color cor_aleatoria_gb(std::default_random_engine &gerador, unsigned r)
 {
     sf::Color aux;
@@ -46,6 +54,8 @@ sf::Color cor_aleatoria_gb(std::default_random_engine &gerador, unsigned r)
     return aux;
 }
 
+// Mistura (blend) duas cores
+// Cada componente da nova cor é a média aritmética entre a componente de cada cor
 sf::Color misturar(sf::Color a, sf::Color b)
 {
     return sf::Color(static_cast<sf::Uint8>(a.r / 2.0f + b.r / 2.0f),
@@ -53,6 +63,8 @@ sf::Color misturar(sf::Color a, sf::Color b)
                      static_cast<sf::Uint8>(a.b / 2.0f + b.b / 2.0f));
 }
 
+// Inverte uma cor
+// Cada componente da nova cor é a subtração entre o valor máximo (255) e a componente da cor
 sf::Color inverter(sf::Color a)
 {
     return sf::Color(static_cast<sf::Uint8>(255 - a.r),
@@ -60,6 +72,7 @@ sf::Color inverter(sf::Color a)
                      static_cast<sf::Uint8>(255 - a.b));
 }
 
+// Calcula a distância cartesiana entre as cores através de suas componentes
 double distancia(sf::Color a, sf::Color b)
 {
     return std::sqrt(std::pow(a.r - b.r, 2) + std::pow(a.g - b.g, 2) + std::pow(a.b - b.b, 2));
